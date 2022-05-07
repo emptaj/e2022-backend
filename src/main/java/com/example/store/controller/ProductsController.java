15cd872cc1cc528e20c8181888dfc1c479ee0271
Tom-theCoder <tom.swirgon@gmail.com>
2022-05-07 13:21:37 +0200

    ProductsService - fix mapping

--- src/main/java/com/example/store/controller/ProductsController.java
+++ src/main/java/com/example/store/controller/ProductsController.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController("/products")
+@RestController
+@RequestMapping(path = "/products")
 @Transactional
 @RequiredArgsConstructor
 public class ProductsController {
