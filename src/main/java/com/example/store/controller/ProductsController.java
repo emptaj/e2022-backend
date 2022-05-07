b874083acaeba04554c4c639cc6871e4bbe420c3
Tom-theCoder <tom.swirgon@gmail.com>
2022-05-07 12:05:20 +0200

    ProductsService - createProduct

--- src/main/java/com/example/store/controller/ProductsController.java
+++ src/main/java/com/example/store/controller/ProductsController.java
@@ -7,7 +7,11 @@ import javax.transaction.Transactional;
 import com.example.store.model.ProductModel;
 import com.example.store.service.ProductsService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -23,5 +27,11 @@ public class ProductsController {
     @GetMapping("")
     List<ProductModel> getProducts() {
         return service.getProducts();
+    }
+
+    @PostMapping("")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    ProductModel createProduct(@RequestBody ProductModel product) {
+        return service.createProduct(product);
     }
 }
--- src/main/java/com/example/store/service/ProductsService.java
+++ src/main/java/com/example/store/service/ProductsService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.example.store.converter.ProductsConverter;
+import com.example.store.entity.ProductEntity;
+import com.example.store.exception.ValidationException;
 import com.example.store.model.ProductModel;
 import com.example.store.repository.ProductRepository;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,9 +21,31 @@ public class ProductsService {
     private final ProductRepository repository;
     private final ProductsConverter converter;
 
+
     public List<ProductModel> getProducts() {
         return repository.findAll().stream()
                 .map(converter::toModel)
                 .collect(Collectors.toList());
-    }    
+    }
+    
+
+    public ProductModel createProduct(ProductModel product) {
+        validateInput(product);
+        ProductEntity entity = converter.create(product);
+        entity = repository.save(entity);
+        return converter.toModel(entity);
+    }
+
+    private void validateInput(ProductModel product) {
+        if (!StringUtils.hasText(product.getName()))
+            throw new ValidationException("Product name cannot be empty");
+        if (!StringUtils.hasText(product.getDescription()))
+            throw new ValidationException("Product description cannot be empty");
+        if (product.getWarehouseId() == null)
+            throw new ValidationException("Product warehouse cannot be empty");
+        if (product.getPrice() == null)
+            throw new ValidationException("Product price cannot be empty");
+        if (product.getPrice() < 0.0f)
+            throw new ValidationException("Product price cannot be negative");
+    }
 }
