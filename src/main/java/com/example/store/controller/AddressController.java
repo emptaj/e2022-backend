e6ce9a322fae76e745da168a7567c86ca3700abd
Marcin Szyszka <toja@example.com>
2022-05-24 17:34:49 +0200

    SecurityConfig: endpoints only for authenticated users

--- src/main/java/com/example/store/controller/AddressController.java
+++ src/main/java/com/example/store/controller/AddressController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/addresses")
+@RequestMapping("/api/addresses")
 @AllArgsConstructor
 public class AddressController {
 
