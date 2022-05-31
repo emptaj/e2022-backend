05c07c94d0c8af0a90dd49fdc7a6f6814ee9290f
Marcin Szyszka <toja@example.com>
2022-05-31 17:22:47 +0200

    Fix warehouseId: id generation strategy

--- src/main/java/com/example/store/entity/WarehouseEntity.java
+++ src/main/java/com/example/store/entity/WarehouseEntity.java
@@ -2,11 +2,7 @@ package com.example.store.entity;
 
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,7 +19,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 @AllArgsConstructor
 public class WarehouseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
