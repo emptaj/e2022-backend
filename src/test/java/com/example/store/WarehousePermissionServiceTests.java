80235b2954307fc98f5b7379d20a56ff83d51c7c
PLP <r.t.szymanek@gmail.com>
2022-06-05 01:09:34 +0200

    Firt WarehousePermission Tests

---
+++ src/test/java/com/example/store/WarehousePermissionServiceTests.java
@@ -0,0 +1,46 @@
+package com.example.store;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.store.Builder.ExampleDTOBuilder;
+import com.example.store.dto.warehouse.WarehouseDTO;
+import com.example.store.exception.NotFoundException;
+import com.example.store.service.WarehousePermissionService;
+import com.example.store.service.WarehouseService;
+
+@SpringBootTest
+public class WarehousePermissionServiceTests {
+    
+    @Autowired
+    private WarehouseService warehouseService;
+    @Autowired
+    private WarehousePermissionService warehousePermissionService;
+
+    @Test
+    @Transactional
+    void CreatePermisionForExistingWarehouseTest(){
+        WarehouseDTO warehouse = warehouseService.createWarehouse(ExampleDTOBuilder.buildExampleWarehouseDTO());
+        assertTrue(warehousePermissionService.getForWarehouse(warehouse.getId()).isEmpty());
+        
+    }
+
+    @Test
+    @Transactional
+    void CreatePermisionForNONExistingWarehouseTest() throws NotFoundException{
+        
+        assertThrows(NotFoundException.class, () -> {
+            warehousePermissionService.getForWarehouse(-1L);
+        });    
+
+        
+    }
+
+
+}
