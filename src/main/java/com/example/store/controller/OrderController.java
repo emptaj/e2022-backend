f60b36cb6fd6279220c88b6aa8b3ec8d9b2c0e12
Tom-theCoder <tom.swirgon@gmail.com>
2022-06-04 13:47:08 +0200

    refactoring: missing response satatus in orderController

--- src/main/java/com/example/store/controller/OrderController.java
+++ src/main/java/com/example/store/controller/OrderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.store.dto.ListDTO;
@@ -36,6 +38,7 @@ public class OrderController {
     }
 
     @PostMapping("/orders")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public List<OrderDTO> createOrder(@RequestBody CreateOrderDTO dto) {
         return service.createOrder(dto);
     }
