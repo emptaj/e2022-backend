a329b08bcea5b8e150ccd7b3b20657be024c5ff6
Tom-theCoder <tom.swirgon@gmail.com>
2022-06-05 15:32:30 +0200

    DeliveryType-Service: add modification operator

--- src/main/java/com/example/store/mapper/DeliveryTypeMapper.java
+++ src/main/java/com/example/store/mapper/DeliveryTypeMapper.java
@@ -13,6 +13,7 @@ import com.example.store.dto.deliveryType.UpdateDeliveryTypeDTO;
 import com.example.store.dto.deliveryType.CreateDeliveryTypeDTO;
 import com.example.store.entity.AddressEntity;
 import com.example.store.entity.DeliveryTypeEntity;
+import com.example.store.entity.UserEntity;
 
 
 @Mapper
@@ -23,17 +24,28 @@ public interface DeliveryTypeMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "active", constant = "true")
     @Mapping(target = "address", source = "address")
+    @Mapping(target = "modificationDate", ignore = true)
+    @Mapping(target = "modificationOperator", ignore = true)
     DeliveryTypeEntity create(CreateDeliveryTypeDTO dto, AddressEntity address);
 
     @Mapping(target = "addressId", source = "address.id")
     DeliveryTypeDTO toDTO(DeliveryTypeEntity entity);
 
     @Mapping(target = "addressId", source = "address.id")
+    @Mapping(target = "modificationOperatorId", source = "modificationOperator.id")
     DeliveryTypeExDTO toExDTO(DeliveryTypeEntity entity);
 
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "active", constant = "false")
-    DeliveryTypeEntity delete(@MappingTarget DeliveryTypeEntity entity, LocalDate modificationDate);
+    @Mapping(target = "modificationDate", source = "date")
+    @Mapping(target = "modificationOperator", source = "operator")
+    DeliveryTypeEntity delete(@MappingTarget DeliveryTypeEntity entity, UserEntity operator, LocalDate date);
 
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "address", ignore = true)
-    DeliveryTypeEntity update(@MappingTarget DeliveryTypeEntity entity, UpdateDeliveryTypeDTO dto);
+    @Mapping(target = "email", source = "dto.email")
+    @Mapping(target = "modificationDate", source = "date")
+    @Mapping(target = "modificationOperator", source = "operator")
+    DeliveryTypeEntity update(@MappingTarget DeliveryTypeEntity entity, UpdateDeliveryTypeDTO dto,
+                              UserEntity operator, LocalDate date);
 }
