882f0b4fc135bf0bba4006d8a8518bb3a476f2b5
PLP <r.t.szymanek@gmail.com>
2022-06-08 21:24:39 +0200

    Fix

--- src/test/java/com/example/store/WarehouseServiceTests.java
+++ src/test/java/com/example/store/WarehouseServiceTests.java
@@ -49,7 +49,7 @@ class WarehouseServiceTests {
 
     @Test
     @Transactional
-    @WithMockUser
+    @WithMockUser("admin")
     void createdWarehouseTest(){
         CreateWarehouseDTO warehouse = ExampleDTOBuilder.buildExampleWarehouseDTO();
         WarehouseDTO createdWarehouse = service.createWarehouse(warehouse);
