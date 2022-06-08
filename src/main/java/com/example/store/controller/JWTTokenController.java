08c01cba33c5dbb61760ad85a1ab535461c62061
Marcin Szyszka <toja@example.com>
2022-06-08 14:20:48 +0200

    Creating TokenService to manage tokens and responses

--- src/main/java/com/example/store/controller/JWTTokenController.java
+++ src/main/java/com/example/store/controller/JWTTokenController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/token/")
 public class JWTTokenController {
     private final JWTTokenService tokenService;
-    
+
+    @GetMapping("/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        tokenService.refreshToken(request, response);
+    }
 }
--- src/main/java/com/example/store/service/JWTTokenService.java
+++ src/main/java/com/example/store/service/JWTTokenService.java
@@ -6,13 +6,8 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.store.entity.UserEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -52,5 +47,56 @@ public class JWTTokenService {
                 .withSubject(userDetails.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime * 2))
                 .sign(Algorithm.HMAC256(secret));
+    }
+
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refresh_token = authorizationHeader.substring("Bearer ".length());
+                //settings
+                String username = getDecodedJWT(refresh_token, secret).getSubject();
+
+                //loading user
+                UserDetails userDetails = userService.loadUserByUsername(username);
+                UserEntity userEntity = (UserEntity) userDetails;
+                String access_token = createAccessToken(userDetails);
+
+                //preparing response
+                prepareResponseWithTokens(response, access_token, refresh_token, userEntity.getId());
+
+            } catch (Exception e) {
+                prepareFailResponse(response, e.getMessage());
+            }
+        } else {
+            prepareFailResponse(response, "token is empty");
+        }
+    }
+
+    public DecodedJWT getDecodedJWT(String token, String secret) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        return verifier.verify(token);
+
+    }
+
+    public void prepareResponseWithTokens(HttpServletResponse response,
+                                          String access_token,
+                                          String refresh_token, Long userId) throws IOException {
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", "Bearer " + access_token);
+        tokens.put("refresh_token", "Bearer " + refresh_token);
+        tokens.put("user_id", String.valueOf(userId));
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setHeader("Authorization", "Bearer " + access_token);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+    }
+
+    public void prepareFailResponse(HttpServletResponse response, String errorMessage) throws IOException {
+        response.setStatus(FORBIDDEN.value());
+        Map<String, String> error = new HashMap<>();
+        error.put("error_message", errorMessage);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), error);
     }
 }
