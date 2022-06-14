dcaae89b9365159a77fe2409027689c6fdae2495
Tom-theCoder <tom.swirgon@gmail.com>
2022-06-14 18:37:03 +0200

    PayU: send request on order create

--- src/main/java/com/example/store/service/PayuService.java
+++ src/main/java/com/example/store/service/PayuService.java
@@ -3,25 +3,40 @@ package com.example.store.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.store.dto.payu.PayuBuyerDTO;
 import com.example.store.dto.payu.PayuCreateOrderDTO;
+import com.example.store.dto.payu.PayuCreateOrderResponseDTO;
 import com.example.store.dto.payu.PayuProductDTO;
 import com.example.store.entity.OrderEntity;
 import com.example.store.entity.UserEntity;
 
-import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
 public class PayuService {
 
     private final UserService userService;
+    private RestTemplate restTemplate;
+
+    private static final String createOrderURL = "https://payu.pl"; // TODO: SET
+
+
+    public PayuService(UserService userService) {
+        this.userService = userService;
+        this.restTemplate = new RestTemplate();
+    }
 
 
     public void sendOrder(OrderEntity order) {
         PayuCreateOrderDTO body = createPayuOrder(order);
+
+        ResponseEntity<PayuCreateOrderResponseDTO> response = restTemplate.postForEntity(
+                createOrderURL, body, PayuCreateOrderResponseDTO.class);
+        if (!response.getBody().getStatus().getStatus().equals("SUCCESS"))
+            System.err.println("PayU create order Failed");
     }
     
     private PayuCreateOrderDTO createPayuOrder(OrderEntity order) {
@@ -38,6 +53,7 @@ public class PayuService {
                     Integer.toString(detail.getQuantity())
                 ))
                 .collect(Collectors.toList());
+
         return PayuCreateOrderDTO.builder()
                 .notifyUrl("http://localhost:8080/orders/notify-payment")
                 .customerIp("") // TODO: SET
